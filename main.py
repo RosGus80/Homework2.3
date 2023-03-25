a193b09a002a4705a99e81319650f8216aeba46c
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2023-03-25 11:39:27 +0300

    Cleaned a cide

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import json
 import random
 
+
 class Question():
-    def __init__(self, text, difficulty, answer, IsCalled=False, UserAnswer=None):
+    def __init__(self, text, difficulty, answer):
         self.text = text
         self.difficulty = difficulty
         self.answer = answer
-        self.IsCalled = IsCalled
-        self.UserAnswer = UserAnswer
+        self.IsCalled = False
+        self.UserAnswer = None
         self.points = int(self.difficulty) * 10
 
-
     def get_points(self):
-        """Возвращает int, количество баллов."""
+        """Возвращает int количество баллов."""
 
         return self.points
 
@@ -26,9 +26,8 @@ class Question():
         else:
             self.UserAnswer = False
 
-
     def build_question(self):
-        """Возвращает вопрос в понятном пользователю виде, например:
+        """Печатает вопрос в понятном пользователю виде, например:
         Вопрос: What do people often call American flag?
         Сложность 4/5
         """
@@ -37,11 +36,10 @@ class Question():
         print(f"Сложность {self.difficulty}/5")
 
     def build_feedback(self):
-        if self.UserAnswer == True:
+        if self.UserAnswer:
             print(f"Ответ верный, получено {self.points} баллов")
         else:
             print(f"Ответ неверный, верный ответ {self.answer}")
-
 
 
 def main():
@@ -62,25 +60,25 @@ def main():
         # Выбираю случайный элемент из списка вопросов, но, поскольку программа не может повториться в вопросах,
         # мне нжно находить случайный индекс и добавлять в список индексов, которые уже были использованы (поэтому random.choice нельзя использовать)
         while True:
-            question_index = random.randint(0, (len(questions)-1))
+            question_index = random.randint(0, (len(questions) - 1))
             if question_index not in questions_asked:
                 break
 
         questions_asked.append(question_index)
         question = questions[question_index]
 
-
         question.build_question()
         question.is_correct(input())
         question.build_feedback()
 
     for question in questions:
-        if question.UserAnswer == True:
+        if question.UserAnswer:
             correct_answers += 1
             points += question.get_points()
     print("Вот и всё!")
     print(f"Отвечено {correct_answers} вопроса из {len(questions)}")
     print(f"Набрано баллов: {points}")
+
 
 
 main()
